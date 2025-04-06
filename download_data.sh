7469a454b66dd7dca45ee821df8a3040747173c7
OkusamaOwO <lanhuong2k4UwU@gmail.com>
2025-04-06 21:50:52 +0700

    pycharm bi sao v, try to set up branch for gpu training

---
+++ download_data.sh
@@ -0,0 +1,5 @@
+# run sequentially
+# run the first line, after that run the second
+
+#gdown --folder 'https://drive.google.com/drive/u/2/folders/1_Z6bkOlNxQ-pcYEtX8mjZkWNEDclLiic' -O cloud_dataset
+#unzip ./cloud_dataset/50sample_data.zip
